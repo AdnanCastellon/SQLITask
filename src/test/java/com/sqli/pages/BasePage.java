564cf728605342385a83d751b449a44d8b5eaf0d
Saadet <99473429+sakoyaespana@users.noreply.github.com>
2024-04-15 11:13:24 +0200

    Adding webdriver manager dependency

---
+++ src/test/java/com/sqli/pages/BasePage.java
@@ -0,0 +1,12 @@
+package com.sqli.pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import java.sql.Driver;
+
+public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+}
