d6d210706a46c87eb9506f9035f868c0283f205c
Saadet <99473429+sakoyaespana@users.noreply.github.com>
2024-04-15 11:11:05 +0200

    Adding pool driver class

---
+++ src/test/java/com/sqli/utilities/Driver.java
@@ -0,0 +1,54 @@
+package com.sqli.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private Driver(){}
+
+    // private static WebDriver driver; // null by default, Encapsulation, I canceled single driver
+
+    private static InheritableThreadLocal<WebDriver>  driverPool  = new InheritableThreadLocal<>();
+
+
+
+
+
+    public static WebDriver getDriver(){
+
+        if(driverPool.get() == null){  // sayet null sa browser'i yenider kuruyoruz.
+            String str=ConfigurationReader.getProperty("browser");
+            switch (str){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver());
+
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+
+        return driverPool.get();       // return existing instance driver
+    }
+
+
+    public static void closeDriver(){
+        if(driverPool.get()!=null){
+            driverPool.get().quit();
+
+            driverPool.remove(); // driver=null; instead of this
+        }
+    }
+}
